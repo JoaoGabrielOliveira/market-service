eed515a743d9ef7cfcd3560ca75f7f7143fecbb6
João Gabriel <joaogabrieloliveira81@gmail.com>
2023-01-13 01:23:44 -0300

    refactor (log) : add enum to color font of console

--- src/util/Event.ts
+++ src/util/Event.ts
@@ -1,18 +1,25 @@
 export default async function SendEvent(message : string, data : any, level : string = 'info') : Promise<void>{
     switch(level){
         case 'info':
-            console.log(`[${level}] ${message}`, data);
+            console.log(`${ConsoleColor.FtBlue}[${level}] ${ConsoleColor.FtDefault + message}`, data);
         break;
 
         case 'warn':
-            console.warn(`[${level}] ${message}`, data);
+            console.warn(`${ConsoleColor.FtYellow}[${level}] ${ConsoleColor.FtDefault + message}`, data);
         break;
 
         case 'error':
-            console.error(`[${level}] ${message}`, data);
+            console.error(`${ConsoleColor.FtRed}[${level}] ${ConsoleColor.FtDefault + message}`, data);
         break;
 
         default:
             throw new Error("Send type level is not accept!");
     }
+}
+
+enum ConsoleColor {
+    FtBlue = "\x1b[34m",
+    FtRed = "\x1b[31m",
+    FtYellow = "\x1b[33m",
+    FtDefault = "\x1b[37m"
 }
