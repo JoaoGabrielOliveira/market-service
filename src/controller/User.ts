badb01e5716576775a1463d808f6a036ae5f044e
João Gabriel <joaogabrieloliveira81@gmail.com>
2023-03-07 16:11:54 -0300

    refactor(controller): make controllers return response

--- src/controller/User.ts
+++ src/controller/User.ts
@@ -6,11 +6,11 @@ import { MapperArrayTo } from "../util/Mapper";
 const service = new UserService();
 
 export async function getAllUsers(req : Request, res : Response) {  
-    res.status(200).send(
+    return res.status(200).send(
         MapperArrayTo(UserDAO, await service.find({ order: { firstName: 'ASC'}}))
     );
 };
 
 export async function getOneUser(req : Request, res : Response) {
-    res.status(200).send(await service.findById(req.params.id));
+    return res.status(200).send(await service.findById(req.params.id));
 };
