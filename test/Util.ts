8f93c44905fdb0fa7f28b370b046a0a43aeccb8f
João Gabriel <joaogabrieloliveira81@gmail.com>
2023-03-07 16:13:20 -0300

    test(util): refactoring util script to improve data source initializer

--- test/Util.ts
+++ test/Util.ts
@@ -1,14 +1,14 @@
 import { DataSource } from "typeorm";
+import { SetAndInitializeDataSource} from "../src/database/data-source";
 
-export async function makeInMemoryDatabase(entities : any[]) : Promise<DataSource>{
-    let dataSource = new DataSource({
+export async function makeInMemoryDatabase(entities : any[]) : Promise<boolean>{
+      return SetAndInitializeDataSource(new DataSource({
         type: "sqlite",
         database: ":memory:",
         dropSchema: true,
         entities: entities,
         synchronize: true,
         logging: false
-      });
-
-      return dataSource.initialize();
+      }));
+      
 }
